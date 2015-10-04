b55ab141d2580189d0ce9137a24a559a1d71cba8
Avram Colak <acolak@cs.otago.ac.nz>
2015-10-05 01:02:12 +1300

    Remove pointlessly noisy description in header comment.

--- lisp/wolfram-mode-rc.el
+++ lisp/wolfram-mode-rc.el
@@ -1,5 +1,4 @@
-;; Simple rc to configure wolfram-mode. wolfram-mode is an Emacs
-;; interface to the Mathematica kernel and language.
+;; Simple rc to configure wolfram-mode.
 
 (when (yard-require 'wolfram-mode)
   (add-to-list 'auto-mode-alist '("\\.m$" . wolfram-mode)))
